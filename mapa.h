e6856ab49f6b2d03a96928970231e4b4ece61011
hakuiiki <nacho.placenciap@gmail.com>
2025-11-28 19:40:29 -0300

    nuevas funciones de guardado
    - Se agregan funciones guardarEstado y cargarEstado para... para que mas van a ser por la chucha como me pide explicar toda esta wea github re culiao

--- mapa.h
+++ mapa.h
@@ -10,14 +10,21 @@
 void liberaMemoria(int **matriz, int filas);
 int **reservaMemoria(int filas, int columnas, int conCeros);
 
+
 // Funciones de Mapa
-int **leerArchivo(char *archivo,int filas, int columnas);
 int **generarMapa(int filas, int columnas);
 void generarTanques(int **mapa);
 void dfs(int x, int y, int **mapa, int **visitado, int filas, int columnas);
 int validarMapa(int **mapa, int filas, int columnas);
 int **generarMapaValido(int filas, int columnas);
-void almacenarMapa(int **mapa, int filas, int columnas, char *archivo);
 void imprimirMapa(int **mapa, int filas, int columnas); // Version numerica (debug)
+
+
+// Funciones de lectura y almacenamiento del mapa
+int **leerArchivo(char *archivo,int filas, int columnas);
+void almacenarMapa(int **mapa, int filas, int columnas, char *archivo);
+void guardarEstado(int **mapa, int filas, int columnas);
+int **cargarEstado(int filas, int columnas);
+
 
 #endif
