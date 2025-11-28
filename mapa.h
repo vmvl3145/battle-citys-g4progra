aa024faa877724e9ab059cc5da2c34432ec65fec
hakuiiki <nacho.placenciap@gmail.com>
2025-11-28 20:02:59 -0300

    se añade declaracion de funciones guardarestado y cargarestado
    aparte añado string.h para poder trabajar con el nombre del archivo didacticamente

--- mapa.h
+++ mapa.h
@@ -5,19 +5,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
 // Funciones de Memoria
 void liberaMemoria(int **matriz, int filas);
 int **reservaMemoria(int filas, int columnas, int conCeros);
 
+
 // Funciones de Mapa
-int **leerArchivo(char *archivo,int filas, int columnas);
 int **generarMapa(int filas, int columnas);
 void generarTanques(int **mapa);
 void dfs(int x, int y, int **mapa, int **visitado, int filas, int columnas);
 int validarMapa(int **mapa, int filas, int columnas);
 int **generarMapaValido(int filas, int columnas);
-void almacenarMapa(int **mapa, int filas, int columnas, char *archivo);
 void imprimirMapa(int **mapa, int filas, int columnas); // Version numerica (debug)
+
+
+// Funciones de lectura y almacenamiento del mapa
+int **leerArchivo(char *archivo,int filas, int columnas);
+void almacenarMapa(int **mapa, int filas, int columnas, char *archivo);
+void guardarEstado(int **mapa, int filas, int columnas);
+int **cargarEstado(int filas, int columnas);
+
 
 #endif
